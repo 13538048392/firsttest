394c92a9c018bab28edd8e1cab1099fe5b4abf34
13538048392 <1642504508@qq.com>
2019-01-27 18:24:55 +0800

    lw

--- application/admin/controller/Member.php
+++ application/admin/controller/Member.php
@@ -5,7 +5,18 @@ use think\Controller;
 
 class Member extends Controller{
     public function index(){
-        $memLstData= model('index/member')->lst();
+
+        $where = input('post.data');
+      if($where){
+        parse_str($where,$output);
+        $where =array_filter($output);
+         return $memLstData= model('index/member')->lst($where);
+//          halt($where);
+      }
+//
+//        halt($where);
+
+        $memLstData= model('index/member')->lst($where);
 //        halt($memLstData);
         $this->assign('data',$memLstData);
         return view('index');
